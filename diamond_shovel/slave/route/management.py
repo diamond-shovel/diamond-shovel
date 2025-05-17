02be6d2ff4b61f61e5514d48d99d475432ba2a1c
astro-angelfish <astroangelfish@orangemc.moe>
2025-05-17 11:43:54 +0800

    feat: add remote plugin configuration

--- diamond_shovel/slave/route/management.py
+++ diamond_shovel/slave/route/management.py
@@ -1,9 +1,10 @@
+import configparser
 import os
 import shutil
 import signal
 import sys
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, HTTPException
 from fastapi.params import Depends
 from kink import di
 
@@ -34,3 +35,19 @@ async def install_plugin(file: UploadFile, data_path = Depends(lambda: di["data_
 def uninstall_plugin(plugin_name: str, data_path = Depends(lambda: di["data_path"])):
     shutil.rmtree(data_path / "plugins" / plugin_name)
     (data_path / "plugins" / plugin_table['file']).unlink()
+
+@router.post('/plugin/{plugin_name}')
+def configure_plugin(plugin_name: str, plugin_config: dict, data_path = Depends(lambda: di["data_path"])):
+    if not (data_path / "plugins" / plugin_name).exists():
+        raise HTTPException(status_code=404, detail="Plugin not found")
+    config_file = data_path / "plugins" / plugin_name / "config.ini"
+    if not config_file.exists():
+        raise HTTPException(status_code=404, detail="Config file not found")
+
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    for section, (option, value) in plugin_config.items():
+        config.set(section, option, value)
+
+    config.write(config_file)
