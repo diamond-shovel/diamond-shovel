667b564460cd093372fd4effc19f4ff51aa7600a
astro-angelfish <astroangelfish@orangemc.moe>
2025-05-28 07:02:29 +0800

    feat: multiple peer verification

--- diamond_shovel/slave/security.py
+++ diamond_shovel/slave/security.py
@@ -16,52 +16,57 @@ from fastapi.params import Header
 from kink import inject
 
 _private_key: ec.EllipticCurvePrivateKey
-_peer_public_key: ec.EllipticCurvePublicKey | None = None
-
-verified_count = 0
-signed_count = 0
+_peer_keys: dict[str, tuple[ec.EllipticCurvePublicKey, int, int]] = {}
 
 @inject
 def init(run_context):
     global _private_key
-    global _peer_public_key
+    global _peer_keys
     with open(run_context["root"] / "keys" / "private_key.pem", "rb") as f:
         _private_key = serialization.load_pem_private_key(f.read(), None)
 
-    public_key_path: pathlib.Path = run_context["root"] / "keys" / "peer_key.pem"
-    if public_key_path.exists():
-        with open(public_key_path, "rb") as f:
-            _peer_public_key = serialization.load_pem_public_key(f.read())
-    else:
-        # TODO: add a warning message, i don't have any input method yet
-        pass
+    public_key_path: pathlib.Path = run_context["root"] / "keys" / "peer"
+    for key_file in public_key_path.iterdir():
+        if not key_file.is_file():
+            continue
+        if not key_file.name.endswith(".pem"):
+            continue
+        with open(key_file, "rb") as f:
+            _peer_keys[key_file.name] = serialization.load_pem_public_key(f.read()), 0, 0
 
 async def validate_peer_signature(request: Request, x_signature: Annotated[str | None, Header()] = None):
-    global verified_count
-
-    if _peer_public_key is None:
+    if len(_peer_keys) == 0:
         return
     if x_signature is None:
         raise HTTPException(status_code=403, detail="Invalid signature")
 
-    sha = hashlib.sha256()
-    sha.update(struct.pack("<I", verified_count))
-    sha.update(request.url.path.encode("utf8"))
-    sha.update(await request.body())
-    result = sha.digest()
-    try:
-        _peer_public_key.verify(base64.b64decode(x_signature), result, ECDSA(Prehashed(SHA256())))
-    except InvalidSignature:
-        raise HTTPException(status_code=403, detail="Invalid signature")
-    verified_count += 1
+    verified = False
+    for key_name, (key, verified_count, signed_count) in _peer_keys.items():
+        sha = hashlib.sha256()
+        sha.update(struct.pack("<I", verified_count))
+        sha.update(request.url.path.encode("utf8"))
+        sha.update(await request.body())
+        result = sha.digest()
+        try:
+            key.verify(base64.b64decode(x_signature), result, ECDSA(Prehashed(SHA256())))
+        except InvalidSignature:
+            continue
+        request.state.source = key_name
+        verified = True
+        _peer_keys[key_name] = key, verified_count + 1, signed_count
+        break
 
-async def sign_response(response: Response):
-    global signed_count
+    if not verified:
+        raise HTTPException(status_code=403, detail="Invalid signature")
+
+async def sign_response(request: Request, response: Response):
     yield
+
+    key, verified_count, signed_count = _peer_keys[request.state.source]
 
     sha = hashlib.sha256()
     sha.update(struct.pack("<I", signed_count))
     sha.update(await response.body())
     result = sha.digest()
     response.headers['X-Signature'] = str(base64.b64encode(_private_key.sign(result, ECDSA(Prehashed(SHA256())))))
-    signed_count += 1
+    _peer_keys[request.state.source] = key, signed_count + 1, signed_count
