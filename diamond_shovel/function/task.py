fbdc126d72247fddc823e78acda8f8731d851561
astro-angelfish <astroangelfish@orangemc.moe>
2025-06-20 13:51:07 +0800

    feat: capture exception inside `WorkerPool#run_worker`, so that we can log exception to http api.

--- diamond_shovel/function/task.py
+++ diamond_shovel/function/task.py
@@ -3,6 +3,7 @@ import copy
 import inspect
 import logging
 import random
+import traceback
 from asyncio import Future, current_task
 from typing import Callable, Any, Coroutine
 
@@ -362,26 +363,30 @@ class WorkerPool:
             if isinstance(ctx_target_companies, TaskContext) \
             else await initialize_task_context(ctx_target_companies, target_domains, target_ips)
 
-        async with asyncio.TaskGroup() as tg:
-            await async_helper.call_sync(events.call_event, events.TaskDispatchEvent(ctx))
-            all_tasks = CoroutineQueue()
-            for plugin_ctx, workers in self._workers.items():
-                if not manage.is_plugin_enabled(plugin_ctx.plugin_name):
-                    continue
-                for worker, nice in workers:
-                    if not ctx.filter_worker(plugin_ctx, worker):
+        try:
+            async with asyncio.TaskGroup() as tg:
+                await async_helper.call_sync(events.call_event, events.TaskDispatchEvent(ctx))
+                all_tasks = CoroutineQueue()
+                for plugin_ctx, workers in self._workers.items():
+                    if not manage.is_plugin_enabled(plugin_ctx.plugin_name):
                         continue
+                    for worker, nice in workers:
+                        if not ctx.filter_worker(plugin_ctx, worker):
+                            continue
 
-                    logging.info(f"Dispatched worker {worker.__qualname__} for {plugin_ctx.plugin_name}")
-                    task = ShovelCoroutine(plugin_ctx, worker, ctx, tg, nice)
-                    all_tasks.put(task)
-            ctx.start(all_tasks)
+                        logging.info(f"Dispatched worker {worker.__qualname__} for {plugin_ctx.plugin_name}")
+                        task = ShovelCoroutine(plugin_ctx, worker, ctx, tg, nice)
+                        all_tasks.put(task)
+                ctx.start(all_tasks)
 
-            if all_tasks.size() == 0:
-                logging.warning("No workers available.")
-                return "No worker to run."
+                if all_tasks.size() == 0:
+                    logging.warning("No workers available.")
+                    return "No worker to run."
 
-            return await ctx.get_all_results()
+                return await ctx.get_all_results()
+        except Exception:
+            logging.error(f"Error while running workers for context {ctx}: {traceback.format_exc()}")
+            return "Errored"
 
 
 worker_pool = WorkerPool()
